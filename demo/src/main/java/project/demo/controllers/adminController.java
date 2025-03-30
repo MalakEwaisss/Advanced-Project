2ca7f300dda263b0c588bd48881ed7f083e1bfe2
Saradiaa20 <sara2201921@miuegypt.edu.eg>
2025-03-30 17:30:48 +0200

    commented error files

--- demo/src/main/java/project/demo/controllers/adminController.java
+++ demo/src/main/java/project/demo/controllers/adminController.java
@@ -1,61 +1,61 @@
-package project.demo.controllers;
+// package project.demo.controllers;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.DeleteMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.PutMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-import project.demo.IAdminActions;
-import project.demo.models.User;
-import project.demo.repository.adminRepository;
+// import project.demo.IAdminActions;
+// import project.demo.models.User;
+// import project.demo.repository.adminRepository;
 
-@RestController
-@RequestMapping("/api/admin")
-public class adminController {
-    private final IAdminActions adminActions;
-    private final adminRepository adminRepository;
+// @RestController
+// @RequestMapping("/api/admin")
+// public class adminController {
+//     private final IAdminActions adminActions;
+//     private final adminRepository adminRepository;
 
-    public adminController(IAdminActions adminActions, adminRepository adminRepository) {
-        this.adminActions = adminActions;
-        this.adminRepository = adminRepository;
-    }
+//     public adminController(IAdminActions adminActions, adminRepository adminRepository) {
+//         this.adminActions = adminActions;
+//         this.adminRepository = adminRepository;
+//     }
 
-    @PostMapping("/users")
-    public ResponseEntity<User> createUser(@RequestBody User user) {
-        User createdUser = adminActions.createUser(user);
-        adminRepository.saveUser(createdUser);
-        return ResponseEntity.ok(createdUser);
-    }
+//     @PostMapping("/users")
+//     public ResponseEntity<User> createUser(@RequestBody User user) {
+//         User createdUser = adminActions.createUser(user);
+//         adminRepository.saveUser(createdUser);
+//         return ResponseEntity.ok(createdUser);
+//     }
 
-    @DeleteMapping("/users/{userId}")
-    public ResponseEntity<Void> deleteUser(@PathVariable String userId) {
-        adminActions.deleteUser(userId);
-        adminRepository.deleteUser(userId);
-        return ResponseEntity.noContent().build();
-    }
+//     @DeleteMapping("/users/{userId}")
+//     public ResponseEntity<Void> deleteUser(@PathVariable String userId) {
+//         adminActions.deleteUser(userId);
+//         adminRepository.deleteUser(userId);
+//         return ResponseEntity.noContent().build();
+//     }
 
-    @PutMapping("/tasks/{taskId}")
-    public ResponseEntity<Task> editTask(@PathVariable String taskId, @RequestBody Task task) {
-        Task updatedTask = adminActions.editAnyTask(taskId, task);
-        adminRepository.saveTask(updatedTask);
-        return ResponseEntity.ok(updatedTask);
-    }
+//     @PutMapping("/tasks/{taskId}")
+//     public ResponseEntity<Task> editTask(@PathVariable String taskId, @RequestBody Task task) {
+//         Task updatedTask = adminActions.editAnyTask(taskId, task);
+//         adminRepository.saveTask(updatedTask);
+//         return ResponseEntity.ok(updatedTask);
+//     }
 
-    @DeleteMapping("/tasks/{taskId}")
-    public ResponseEntity<Void> deleteTask(@PathVariable String taskId) {
-        adminActions.deleteAnyTask(taskId);
-        adminRepository.deleteTask(taskId);
-        return ResponseEntity.noContent().build();
-    }
+//     @DeleteMapping("/tasks/{taskId}")
+//     public ResponseEntity<Void> deleteTask(@PathVariable String taskId) {
+//         adminActions.deleteAnyTask(taskId);
+//         adminRepository.deleteTask(taskId);
+//         return ResponseEntity.noContent().build();
+//     }
 
-    @PostMapping("/calendars")
-    public ResponseEntity<Calendar> createCalendar(@RequestBody Calendar calendar) {
-        Calendar createdCalendar = adminActions.createOrganizationCalendar(calendar);
-        adminRepository.saveCalendar(createdCalendar);
-        return ResponseEntity.ok(createdCalendar);
-    }
-}
+//     @PostMapping("/calendars")
+//     public ResponseEntity<Calendar> createCalendar(@RequestBody Calendar calendar) {
+//         Calendar createdCalendar = adminActions.createOrganizationCalendar(calendar);
+//         adminRepository.saveCalendar(createdCalendar);
+//         return ResponseEntity.ok(createdCalendar);
+//     }
+// }
