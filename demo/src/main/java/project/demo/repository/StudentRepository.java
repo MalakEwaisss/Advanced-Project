94b281a29ddb7cf3fc2868e7baa47dba3714dab9
Saradiaa20 <132017719+Saradiaa20@users.noreply.github.com>
2025-05-29 11:34:43 +0300

    Update StudentRepository.java

--- demo/src/main/java/project/demo/repository/StudentRepository.java
+++ demo/src/main/java/project/demo/repository/StudentRepository.java
@@ -3,6 +3,10 @@ package project.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import project.demo.models.Students;
+import org.springframework.stereotype.Repository;
 
 public interface StudentRepository extends JpaRepository<Students, Long> {
+  Students findByEmail(String email);
+
+    void save(Students student);
 }
