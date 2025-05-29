edb845a78ad2b87e3e02680408dc23d394876817
Saradiaa20 <132017719+Saradiaa20@users.noreply.github.com>
2025-05-29 11:31:26 +0300

    Update Students.java

--- demo/src/main/java/project/demo/models/Students.java
+++ demo/src/main/java/project/demo/models/Students.java
@@ -1,14 +1,14 @@
 package project.demo.models;
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+
 
 @Entity
-@Table(name = "students")  // Correct table name as per your database
-public class Students {
+public class Students extends User{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,55 +20,64 @@ public class Students {
     private String email;
     private String password;
 
-    // Getters and Setters
+    // constructor
+    public Students() {}
+
+    public Students(String firstName, String lastName, String username, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 
 
     // Getters and setters
-
+    @Override
     public Long getId() {
         return id;
     }
-
+    @Override
     public void setId(Long id) {
         this.id = id;
     }
-
+    @Override
     public String getFirstName() {
         return firstName;
     }
-
+    @Override
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
+    @Override
     public String getLastName() {
         return lastName;
     }
-
+    @Override
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
+    @Override
     public String getUsername() {
         return username;
     }
-
+    @Override
     public void setUsername(String username) {
         this.username = username;
     }
-
+    @Override
     public String getEmail() {
         return email;
     }
-
+    @Override
     public void setEmail(String email) {
         this.email = email;
     }
-
+    @Override
     public String getPassword() {
         return password;
     }
-
+    @Override
     public void setPassword(String password) {
         this.password = password;
     }
